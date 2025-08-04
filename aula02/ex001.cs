4ee350cf9b968cc183917294fb6de0d3e72ea1e2
Gustavo Stacoviaki Grassi <106496948+grassiii@users.noreply.github.com>
2025-08-04 11:30:09 -0300

    Add files via upload
    baixar dotnet 8.0.0

---
+++ aula02/ex001.cs
@@ -0,0 +1,12 @@
+﻿using System;
+class ex001{
+
+    static void Main (){
+
+        Console.WriteLine("Digite sua idade: ");
+            int idade = int.Parse(Console.ReadLine());
+        Console.WriteLine("Digite seu nome: ");
+            string nome = Console.ReadLine();
+        Console.WriteLine($"Nome: {nome}\nIdade: {idade}");
+    }
+}
