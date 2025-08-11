61c63387d154b868f44fedf25256da40548e4647
Gustavo Stacoviaki Grassi <106496948+grassiii@users.noreply.github.com>
2025-08-11 11:14:30 -0300

    Add files via upload

---
+++ ex003.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Digite sua idade: ");
+int idade = int.Parse(Console.ReadLine());
+
+if (idade < 18)
+{
+    Console.WriteLine("Você é menor de idade.");
+}
+else
+{
+    Console.WriteLine("Você é maior de idade.");
+}
