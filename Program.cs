c7bb3f7ea4e232f27a7fb85f982e40561fec601b
Gustavo Stacoviaki Grassi <106496948+grassiii@users.noreply.github.com>
2025-08-11 11:08:26 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite seu nome: ");
+            string nome = Console.ReadLine();
+            
+        Console.WriteLine($"Olá {nome}, seja bem vindo ao C#!");
+    }
+}
