f22712bbd4bee900acb33963fae6fbbff1f697e0
Gustavo Stacoviaki Grassi <106496948+grassiii@users.noreply.github.com>
2025-08-11 11:12:15 -0300

    Add files via upload

---
+++ ex002.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Digite um número inteiro: ");
+int n1 = int.Parse(Console.ReadLine());
+Console.Write("Digite outro número inteiro: ");
+int n2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"{n1} + {n2} = {n1 + n2}");
