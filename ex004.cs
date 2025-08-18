793dafbdf9da1b115e04b04e5636ca0906f70a87
Gustavo Stacoviaki Grassi <106496948+grassiii@users.noreply.github.com>
2025-08-18 09:18:15 -0300

    Add files via upload

---
+++ ex004.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Digite a temperatura em celsius: ");
+int graus_c = int.Parse(Console.ReadLine());
+int graus_f = graus_c * 9 / 5 + 32;
+
+Console.WriteLine($"Graus convertido para Fahrenheit: {graus_f}");
